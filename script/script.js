ed646420757ec1be6229e4480e47eb9e2ffc4c76
Maria Pospelova <my.pospelova@gmail.com>
2020-08-05 21:08:23 +0300

    ПР4 v5

---
+++ script/script.js
@@ -0,0 +1,7 @@
+const popup = document.querySelector('.popup');
+const showPopup = function () {
+  popup.classList.add('popup_opened');
+}
+const hidePopup = function () {
+  popup.classList.remove('popup_opened');
+}
